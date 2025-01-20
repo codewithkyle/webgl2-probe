42106a2d8dd546f589aefb8f9f82e127311330ea
Kyle Andrews <codingwithkyle@gmail.com>
2025-01-20 11:00:16 -0500

    Added shader compiler

--- main.js
+++ main.js
@@ -5,11 +5,42 @@ if (gl === null) {
     throw new Error("WebGL2 is not supported by this browser.");
 }
 
+const vsSource = `
+    attribute vec4 aVertexPosition;
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+
+    void main() {
+        gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    }
+`;
+
+const fsSource = `
+    void main() {
+        gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+    }
+`;
+
+const vs = compile_shader(vsSource, gl.VERTEX_SHADER);
+const fs = compile_shader(fsSource, gl.FRAGMENT_SHADER);
+
 gl.clearColor(...hex_to_rgbaf("#181818FF"));
 gl.clear(gl.COLOR_BUFFER_BIT);
 
+function compile_shader(source, type) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.error(gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        throw new Error("Failed to compile shader.");
+    }
+    return shader;
+}
+
 /**
-* Convers HEX color to RGBA array of floats 0.0 to 1.0
+* Convers HEX color to RGBA float[] (0.0 to 1.0)
 * @param {string} hex 
 */
 function hex_to_rgbaf(hex) {
@@ -25,7 +56,7 @@ function hex_to_rgbaf(hex) {
 }
 
 /**
-* Convers HEX color to RGBA array of ints.
+* Convers HEX color to RGBA number[] (0 to 255).
 * @param {string} hex 
 */
 function hex_to_rgbai(hex) {
