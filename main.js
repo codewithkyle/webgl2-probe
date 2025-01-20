a669bfd67e90360afbc80d6b70329dd80114f871
Kyle Andrews <codingwithkyle@gmail.com>
2025-01-20 16:00:21 -0500

    Added blur

--- main.js
+++ main.js
@@ -19,31 +19,59 @@ const vsSource = `
 const fsSource = `
     precision highp float;
 
-    #define WIDTH 800.0
-    #define HEIGHT 600.0
+    #define BACKGROUND_COLOR vec4(0.0, 0.0, 0.0, 1.0)
+    #define FOREGROUND_COLOR vec4(1.0, 1.0, 0.0, 1.0)
 
     uniform vec2 size;
     uniform vec2 pos;
+    uniform float blur;
+
+    bool rect_contains_point(vec2 point, vec2 pos, vec2 size)
+    {
+        return pos.x <= point.x && point.x < pos.x + size.x 
+            && pos.y <= point.y && point.y < pos.y + size.y;
+    }
 
     void main() {
-        vec2 p = gl_FragCoord.xy;
+        vec2 point = gl_FragCoord.xy;
 
-        if (pos.x <= p.x && p.x < pos.x + size.x && 
-            pos.y <= p.y && p.y < pos.y + size.y 
-        )
+        if (rect_contains_point(point, pos, size))
         {
-            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+            gl_FragColor = FOREGROUND_COLOR;
         }
         else
         {
-            gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+            float edge = blur;
+
+            if (pos.x <= point.x && point.x < pos.x + size.x) {
+                edge = min(abs(point.y - pos.y),
+                            abs(point.y - (pos.y + size.y)));
+            } else if (pos.y <= point.y && point.y < pos.y + size.y) {
+                edge = min(abs(point.x - pos.x),
+                            abs(point.x - (pos.x + size.x)));
+            } else {
+                edge = min(edge, length(point - (pos + size * vec2(0.0, 0.0))));
+                edge = min(edge, length(point - (pos + size * vec2(1.0, 0.0))));
+                edge = min(edge, length(point - (pos + size * vec2(0.0, 1.0))));
+                edge = min(edge, length(point - (pos + size * vec2(1.0, 1.0))));
+            }
+
+            if (edge < blur) {
+                gl_FragColor = mix(
+                    FOREGROUND_COLOR,
+                    BACKGROUND_COLOR,
+                    smoothstep(0.0, blur, edge)
+                );
+            } else {
+                gl_FragColor = BACKGROUND_COLOR;
+            }
         }
     }
 `;
 
 const vs = compile_shader(gl, vsSource, gl.VERTEX_SHADER);
 const fs = compile_shader(gl, fsSource, gl.FRAGMENT_SHADER);
-const main = new Program(gl, vs, fs, ["size", "pos"]);
+const main = new Program(gl, vs, fs, ["size", "pos", "blur"]);
 const buffer = make_array_buffer(gl, gl.STATIC_DRAW, new Float32Array([
     -1.0, 1.0,
     1.0, 1.0,
@@ -56,22 +84,41 @@ let pos = {
     y: 10.0,
 };
 let velocity = {
-    x: -100.0,
-    y: -100.0,
+    x: -200.0,
+    y: -200.0,
 };
 const width = 100.0;
 const height = 100.0;
+let blur = 0;
 
 let prevTimestamp = undefined;
 function next_frame(timestamp) {
-    const dt = (prevTimestamp - timestamp) * 0.001;
+    const dt = (timestamp - prevTimestamp) * 0.001;
     prevTimestamp = timestamp;
 
-    pos.x += velocity.x * dt;
-    pos.y += velocity.y * dt;
+    pos.x -= velocity.x * dt;
+    pos.y -= velocity.y * dt;
+    if (blur > 0) blur -= 200 * dt;
 
-    if (pos.x < 0 || pos.x + width > 800.0) velocity.x = -velocity.x;
-    if (pos.y < 0 || pos.y + height > 600.0) velocity.y = -velocity.y;
+    if (pos.x < 0) {
+        velocity.x = -velocity.x;
+        blur += 50;
+        pos.x = 0;
+    }
+    else if (pos.x + width > 800.0) {
+        pos.x = 800 - width;
+        blur += 50;
+        velocity.x = -velocity.x;
+    }
+    if (pos.y < 0) {
+        velocity.y = -velocity.y;
+        blur += 50;
+        pos.y = 0;
+    } else if (pos.y + height > 600.0) {
+        velocity.y = -velocity.y;
+        blur += 50;
+        pos.y = 600 - height;
+    }
 
     draw_scene(main, buffer);
 
@@ -118,6 +165,7 @@ function draw_scene(program, buffer) {
     gl.useProgram(program.program);
     gl.uniform2f(program.uniforms['size'], width, height);
     gl.uniform2f(program.uniforms['pos'], pos.x, pos.y);
+    gl.uniform1f(program.uniforms['blur'], blur);
 
     {
         const offset = 0;
